85ef95c2579a45ebbddb589325ef5b9feaaa8b4c
DolicaAkelloEgwel <dolica.akello-egwel@stfc.ac.uk>
2020-06-07 00:31:48 +0100

    First commit

---
+++ main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "add.c"
+
+int main()
+{
+    int num1, num2;
+    float result;
+    
+    char choice;
+
+    printf("Enter first number: ");
+    scanf("%d",&num1);
+    printf("Enter second number: ");
+    scanf("%d",&num2);
+
+    printf("Choose operation to perform (+,-,*,/,%): ");
+    scanf(" %c",&choice);
+
+    result=0;
+    switch(choice)    
+    {
+        case '+':
+            result=add(num1,num2);
+            break;
+             
+        case '-':
+            result=num1-num2;
+            break;
+         
+        case '*':
+            result=num1*num2;
+            break;
+             
+        case '/':
+            result=(float)num1/(float)num2;
+            break;
+             
+        case '%':
+            result=num1%num2;
+            break;
+        default:
+            printf("Invalid operation.\n");
+    }
+ 
+    printf("Result: %d %c %d = %f\n",num1,choice,num2,result);
+    return 0;
+}
